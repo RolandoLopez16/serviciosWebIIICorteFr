6d601fa7e9601908e93766b949d45b23c35d794c
Rolandolopez16 <rolandolopez@unicomfacauca.edu.co>
2024-11-07 11:53:37 -0500

    Mejoras en Menu

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -8,15 +8,15 @@ import { HomeIcon, CarIcon } from 'lucide-react';
 
 const Sidebar = () => {
   return (
-    <div className="w-64 h-full pt-40 bg-red-500 text-white p-5">
+    <div className="w-50 h-full pt-40 bg-red-500 text-white p-5">
       <h2 className="text-2xl font-bold mb-6">ServiApp</h2>
       <nav>
         <ul>
-          <li className="mb-5 flex gap-4"><HomeIcon/>
-            <Link to="/" className="hover:text-gray-400">Inicio</Link>
+          <li className="mb-8 flex gap-4"><HomeIcon/>
+            <Link to="/" className="hover:text-gray-400 font-bold">INICIO</Link>
           </li>
-          <li className="mb-5 flex gap-4"><CarIcon/>
-            <Link to="/conductores" className="hover:text-gray-400">Conductores</Link>
+          <li className="mb-8 flex gap-4"><CarIcon/>
+            <Link to="/conductores" className="hover:text-gray-400 font-bold">CONDUCTORES</Link>
           </li>
                <li className='flex items-center'>
                 <Bottle/>
